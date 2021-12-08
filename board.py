28b594a867f7f4916d48a353ea969a48378460a3
Monica Jin <jinliemail1@gmail.com>
2021-12-07 19:58:16 -0500

    Board Marking functionality added

--- board.py
+++ board.py
@@ -1,8 +1,44 @@
-def print_board(x):
-    for i in x:
+def print_board(brd):
+    for i in brd:
         print(i)
     print("Board printed")
 
 
-board = [['-', '-', '-'], ['-','-','-'], ['-', '-','-']]
+def check_mark(brd, row, col):
+    if row < 0 or row > 2:
+        return False
+    elif col < 0 or col > 2:
+        return False
+    else:
+        for i in brd:
+            if brd[row][col] != "-":
+                return False
+            else:
+                return True
+
+
+def place_mark(brd, row, col, player_id):
+    if not check_mark(brd, row, col):
+        print("Mark cannot be placed here")
+    else:
+        if player_id == 1:
+            brd[row][col] = "X"
+        elif player_id == 2:
+            brd[row][col] = "O"
+
+
+def run(brd, player):
+    row = int(input("Player " + str(player) + ", Please input a row: "))
+    col = int(input("Player " + str(player) + ", Please input a column: "))
+    place_mark(brd, row, col, player)
+    print_board(brd)
+    if player == 1:
+        player = player + 1
+    elif player == 2:
+        player = player - 1
+    run(brd, player)
+
+
+board = [['-', '-', '-'], ['-', '-', '-'], ['-', '-', '-']]
 print_board(board)
+run(board, 1)
