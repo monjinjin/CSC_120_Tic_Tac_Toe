268d51eef3ac6366a68e0b72c4e61aa6a0ee26f5
Monica Jin <jinliemail1@gmail.com>
2021-12-08 00:46:11 -0500

    Board checking functionality added

--- board.py
+++ board.py
@@ -1,7 +1,6 @@
 def print_board(brd):
     for i in brd:
         print(i)
-    print("Board printed")
 
 
 def check_mark(brd, row, col):
@@ -10,11 +9,10 @@ def check_mark(brd, row, col):
     elif col < 0 or col > 2:
         return False
     else:
-        for i in brd:
-            if brd[row][col] != "-":
-                return False
-            else:
-                return True
+        if brd[row][col] != "-":
+            return False
+        else:
+            return True
 
 
 def place_mark(brd, row, col, player_id):
@@ -27,16 +25,59 @@ def place_mark(brd, row, col, player_id):
             brd[row][col] = "O"
 
 
+def check_win(brd):
+    fully_filled = True
+    for i in range(0, 3):
+        for j in range(0, 3):
+            if brd[i][j] == "-":
+                fully_filled = False
+    who_won = 0
+    for i in range(0, 3):
+        if brd[i][0] == brd[i][1] == brd[i][2] == "X":
+            who_won = 1
+        elif brd[i][0] == brd[i][1] == brd[i][2] == "O":
+            who_won = 2
+        else:
+            for j in range(0, 3):
+                if brd[0][j] == brd[1][j] == brd[2][j] == "X":
+                    who_won = 1
+                elif brd[0][j] == brd[1][j] == brd[2][j] == "O":
+                    who_won = 2
+                else:
+                    if brd[0][0] == brd[1][1] == brd[2][2] == "X":
+                        who_won = 1
+                    elif brd[0][0] == brd[1][1] == brd[2][2] == "O":
+                        who_won = 2
+                    else:
+                        if brd[0][2] == brd[1][1] == brd[2][0] == "X":
+                            who_won = 1
+                        elif brd[0][2] == brd[1][1] == brd[2][0] == "O":
+                            who_won = 2
+                        else:
+                            if fully_filled:
+                                who_won = 3
+    return who_won
+
+
 def run(brd, player):
     row = int(input("Player " + str(player) + ", Please input a row: "))
     col = int(input("Player " + str(player) + ", Please input a column: "))
     place_mark(brd, row, col, player)
     print_board(brd)
+
     if player == 1:
         player = player + 1
     elif player == 2:
         player = player - 1
-    run(brd, player)
+
+    if check_win(brd) == 1:
+        print("Player 1 wins!")
+    elif check_win(brd) == 2:
+        print("Player 2 wins!")
+    elif check_win(brd) == 3:
+        print("Draw")
+    else:
+        run(brd, player)
 
 
 board = [['-', '-', '-'], ['-', '-', '-'], ['-', '-', '-']]
